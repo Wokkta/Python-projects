0259b19d41f4912ff20a60b6796b67a0947c57f0
Wokta <awerty17@mail.ru>
2020-12-01 21:13:56 +0300

    1

--- кф_тренировки/G. Командная олимпиада.py
+++ кф_тренировки/G. Командная олимпиада.py
@@ -11,26 +11,12 @@ while ',' in abilities:
 	abilities.remove(abilities[count])
 	count+=1
 print(abilities, 'abilities')
-for i in range(studens//3):
-	count=len(abilities)
-	diff=0
-	for q in range(1,len(abilities)+1):
-		for k in range(1,4):
-
-			if str(k) ==abilities[q-diff]:
-				print(q,' q ' ,q-diff,'q - diff',len(abilities),' len ',comands, 'comands')
-				abilities.remove(str(k))
-				count2=len(abilities)
-				count-=count2
-				diff=count
-				comands.append(q+count)
-				count+=count2
 
 
 
 
 
-
+print(q,' q ' ,q-diff,'q - diff',len(abilities),' len ',comands, 'comands')
 print(len(comands)//3)
 print(abilities,'abilities after')
 print("comands",comands)

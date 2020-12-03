8801abfac6c28fbd9e91be452f43b2536a9ed079
Wokta <awerty17@mail.ru>
2020-12-03 16:41:28 +0300

    H ready

--- кф_тренировки/H. Выборы.py
+++ кф_тренировки/H. Выборы.py
@@ -1,10 +1,5 @@
 # https://codeforces.com/group/XWOeQO4RLM/contest/206799/problem/H
-n,m=list(map(int,input().split()))
-votes_all=[]
-vinners_by_sities=[]
-for i in range(m):
-	vote_city=list(map(int,input().split()))
-	votes_all.append(vote_city)
+
 def vote(array,vinner,counter=0):
 	maximum=-1
 	for i in range(len(array)):
@@ -12,19 +7,40 @@ def vote(array,vinner,counter=0):
 			maximum=array[i]
 			counter=i+1
 	vinner.append(counter)
+
+
+n,m=list(map(int,input().split()))
+
+votes_all=[]
+
+vinners_by_sities=[]
+
+for i in range(m):
+	vote_city=list(map(int,input().split()))
+	votes_all.append(vote_city)
+
 for k in range(m):
 	vote(votes_all[k],vinners_by_sities)
+
 count=[]
+
 maximum=vinners_by_sities.count(vinners_by_sities[0])
+
+
+
 index=vinners_by_sities[0]
 
-print(maximum)
+
 if maximum==len(vinners_by_sities):
-	print(index)
+	pass
+
 else:
  	for i in range(1,len(vinners_by_sities)):
- 		print(maximum)
-		if vinners_by_sities.count(vinners_by_sities[i]) > maximum:
-			maximum=vinners_by_sities.count(vinners_by_sities[i])
-			index=vinners_by_sities[i]
+
+ 		if vinners_by_sities.count(vinners_by_sities[i]) >maximum or vinners_by_sities.count(vinners_by_sities[i])==maximum and vinners_by_sities[i]<index:
+
+ 			maximum=vinners_by_sities.count(vinners_by_sities[i])
+
+ 			index=vinners_by_sities[i]
+
 print(index)

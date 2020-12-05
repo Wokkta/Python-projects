fe8b7858e0f3eb4700438f411338761b22d8434a
Wokta <awerty17@mail.ru>
2020-12-05 18:26:04 +0300

    update ( i)

--- кф_тренировки/I. A и B и ошибки компиляции.py
+++ кф_тренировки/I. A и B и ошибки компиляции.py
@@ -4,10 +4,11 @@ mistakes=list(map(int,input().split()))
 mistakes_mines_one=list(map(int,input().split()))
 mistakes_mines_two=list(map(int,input().split()))
 debugged=[]
-def find_deb(mistakes_kolvo_q,mistakes_q,array):
+def find_deb(mistakes_kolvo_q,mistakes_q,array,count=-1):
 	for i in range(mistakes_kolvo_q):
-		if mistakes_q[i] not in array or mistakes_q[i] in array and mistakes_q.count(mistakes_q[i])!=array.count(mistakes_q[i]) :
+		if mistakes_q[i] not in array or mistakes_q[i] in array and mistakes_q.count(mistakes_q[i])!=array.count(mistakes_q[i]) and mistakes_q.count(mistakes_q[i])!=count :
 			debugged.append(mistakes_q[i])
+			count=mistakes_q.count(mistakes_q[i])+1
 			
 find_deb(mistakes_kolvo,mistakes,mistakes_mines_one)
 

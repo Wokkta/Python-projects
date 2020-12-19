3ff0ea28614253d6c53d061f4268802f463bf850
Wokkta <artem_klesov@mail.ru>
2020-12-19 18:42:52 +0300

    makes mistakes

---
+++ шаг в будущее/в трехмерном.py
@@ -0,0 +1,77 @@
+sphere,paralipipids=map(int,input().split())
+spheres_data=[]
+paralipipids_data=[]
+for i in range(sphere):
+    values=list(map(int,input().split()))
+    spheres_data.append(values)
+for i in range(paralipipids):
+    values=list(map(int,input().split()))
+    paralipipids_data.append(values)
+print(spheres_data)
+print(paralipipids_data)
+maxx=-3000
+maxy=-3000
+maxz=-3000
+minx=3000
+miny=3000
+minz=3000
+countx=0
+county=1
+countz=2
+not_in_paralipipid=[]
+for i in range(paralipipids):
+    for k in range(0,24,1):
+        if countx==0:
+            if paralipipids_data[i][k]>=maxx:
+                maxx=paralipipids_data[i][k]
+            elif paralipipids_data[i][k]<=minx:
+                minx=paralipipids_data[i][k]
+            countz-=1
+            county-=1
+            countx=3
+        elif county==0:
+            if paralipipids_data[i][k]>=maxy:
+                maxy=paralipipids_data[i][k]
+            elif paralipipids_data[i][k]<=miny:
+                miny=paralipipids_data[i][k]
+            countz-=1
+            countx-=1
+            county=3
+        elif countz==0:
+            if paralipipids_data[i][k]>=maxz:
+                maxz=paralipipids_data[i][k]
+            elif paralipipids_data[i][k]<=minz:
+                minz=paralipipids_data[i][k]
+            county-=1
+            countx-=1
+            countz=3
+for i in range(sphere):
+    countx = 0
+    county = 1
+    countz = 2
+    radius=spheres_data[i][-1]
+    for k in range(len(spheres_data[i])-1):
+        position = spheres_data[i][k]
+        if countx==0:
+            if position+radius>maxx or position-radius>maxx or position+radius<minx or position-radius<minx:
+                not_in_paralipipid.append(i)
+                break
+                countx=3
+                county-=1
+                countz-=1
+        elif county ==0:
+            if position+radius>maxy  or position-radius>maxy or position+radius<miny  or position-radius<miny :
+                not_in_paralipipid.append(i)
+                break
+                county =3
+                countx-=1
+                countz-=1
+        elif countz ==0:
+            if position+radius>maxz  or position-radius>maxz or position+radius<minz  or position-radius<minz :
+                not_in_paralipipid.append(i)
+                break
+                countz =3
+                countx-=1
+                county-=1
+for i in range(len(not_in_paralipipid)):
+    print(not_in_paralipipid[i]+1)

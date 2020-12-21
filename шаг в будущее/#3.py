6e66596df4c4379e839e58d68a3cb80eff446e9e
Wokkta <artem_klesov@mail.ru>
2020-12-21 10:53:26 +0300

    1-3 ready

--- шаг в будущее/#3.py
+++ шаг в будущее/#3.py
@@ -1,11 +1,14 @@
 n,m = map(float,input().split())
 values=[]
+values_copy=[]
 summ=0
 ans=[]
 for i in range(int(m)):
     value=float(input())
     values.append(value)
+    values_copy.append(value)
     summ+=value
+
 if float(summ)<n:
     print("NO")
 else:
@@ -16,11 +19,13 @@ else:
         for i in range(len(values)):
             if values[i] > maximum:
                 maximum = values[i]
-                count=i
-        '''print(maximum,' maximum ',values, ' values ',count+1,' index+1')'''
+                count=values_copy.index(values[i])
+        #print(maximum,' maximum ',values, ' values ',count+1,' index+1')
+        #print(count, ' values_copy.index(values[i])  ',values_copy)
         indexs.append(count+1)
         n -= maximum
         values.remove(maximum)
+# everething down is clear
     while indexs:
         min=2 *summ
         for i in range(len(indexs)):
@@ -30,3 +35,16 @@ else:
         indexs.remove(min)
     for i in range(len(ans)):
         print(ans[i])
+'''test input
+5 4
+1
+3.5
+2
+4'''
+'''
+3 5
+1
+0.8
+1.2
+2
+1.5'''

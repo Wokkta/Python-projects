5fc01bb6a10a2a13afb381a17f62c14e65c5e4b0
Wokkta <awerty17@mail.ru>
2020-12-26 09:51:23 +0300

    Update палиндром.py

--- алги/палиндром.py
+++ алги/палиндром.py
@@ -4,16 +4,17 @@ class palindrome():
 		super(palindrome, self).__init__()
 		
 		self.string=string
-	def with_dashes_and_without_elements(string):
-			return string
-	def only_letters_or_nums(string):
+	def with_dashes_and_without_elements(self,string):
+			return self.string
+	def only_letters_or_nums(self,string):
 		elements="`' /.,_=+-)(*&^%$#@!№;:?"
 		for i in elements:
-			if i in string:
+			if i in self.string:
 				print(i)
-				string.remove(i)
-		return(string)
+				self.string=self.string[:self.string.index(i)]+self.string[self.string.index(i)+1:]
+		return(self.string)
 test='123!@#321'
+test=palindrome(test)
 test=test.only_letters_or_nums(test)
 print(test)
 '''not ready'''

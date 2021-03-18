0613c3804ea4c423ffb24ac47d1727db9e673dd4
Wokkta <awerty17@mail.ru>
2021-03-18 23:49:04 +0300

    added

--- шк/prob_tc.py
+++ шк/prob_tc.py
@@ -1,3 +1,22 @@
+#8
+'''
+counter=0
+def four(n):
+	string=''
+	while n>0:
+		string=str(n%4)+string
+		n//=4
+	return string
+def low(n):
+	for i in range(1,len(n)):
+		if int(n[i-1])<int(n[i]):
+			return False
+	return True
+for x in range(100,1000):
+	if low(four(x)):
+		counter+=1
+print(counter)
+'''
 #22
 '''
 for x in range(1,100000):
@@ -74,10 +93,10 @@ for line in f:
 		values.append(line.strip())
 def bag(values,weight):
 '''
-	''' ne smog  :((( '''
+''' ne smog  :((( '''
 #27
 
-	''' из файла вырезаю первую строку'''
+''' из файла вырезаю первую строку'''
 '''
 f = open('27v01_A.txt', 'r')
 mini = 1000000
@@ -94,4 +113,49 @@ if summ % 31 == 0:
     print(summ-mini)
 else:
     print(summ)
+'''
+#24
+'''
+f=open('24 варианты 1-5.txt','r')
+counter=0
+sims=['X','Y','Z']
+string=''
+for line in f:
+	string=string+line.strip()
+counter1=0
+for i in range(1,len(string)):
+	if sims.index(string[i-1])<=sims.index(string[i]):
+		counter1+=1
+	else:
+		if counter1>counter:
+			counter=counter1
+			counter1=0
+print(counter)
+'''
+#17
+'''
+counter=0
+simvol=0
+def ds(n):
+	dels=[]
+	d=2
+	while d**2<=n:
+		if n%d==0:
+			dels.append(d)
+			if n//d>d:
+				dels.append(n//d)
+		d+=1
+	if 6 in dels :
+		sim=[4,5,9,13]
+		for i in sim :
+			if i in dels:
+				return False
+		return True
+	else:
+		return False
+for x in range(1007,8011):
+	if ds(x):
+		counter+=1
+		simvol=x
+print(counter,simvol)
 '''

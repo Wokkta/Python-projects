75db1d5f186a303f6144b00a83d4068e7dbf4040
Wokkta <awerty17@mail.ru>
2021-03-24 22:46:45 +0300

    add

---
+++ шахматы/расчет ходов пешки.py
@@ -0,0 +1,42 @@
+letters = 'abcdefgh'
+numbers = '12345678'
+graph = {}
+for l in letters:
+    for n in numbers:
+        graph[l+n] = set()
+
+
+def add_edge(v1, v2):
+    graph[v1].add(v2)
+    graph[v2].add(v1)
+
+
+for i in range(8):
+    for j in range(8):
+        v1 = letters[i]+numbers[j]
+        v2 = ''
+    # действие, опред цвет пешки, пока не доделал
+    # белые сверху
+        if j < 4:
+            if j+1 < 8:
+                v2 = letters[i]+numbers[j+1]
+                add_edge(v1, v2)
+            if i+1 < 8 and j+1 < 8:
+                 v2 = letters[i+1]+numbers[j+1]
+                 add_edge(v1, v2)
+            if 0 <= i-1 < 8 and j+1 < 8:
+                v2 = letters[i-1]+numbers[j+1]
+                add_edge(v1, v2)
+        else:
+            if j-1>=0:
+                v2 = letters[i]+numbers[j-1]
+                add_edge(v1, v2)
+            if j-1>=0 and 0<=i+1<8:
+                v2 = letters[i+1]+numbers[j-1]
+                add_edge(v1, v2)
+            if j-1>=0 and 0<=i-1<8:
+                v2 = letters[i-1]+numbers[j-1]
+                add_edge(v1, v2)
+
+for i in graph:
+    print(i, ' ', graph[i])
